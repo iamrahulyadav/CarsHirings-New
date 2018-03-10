8ad9ab4c42e1916aef4bbe5a9d19381a7510414b
lenovo <rakhi@askonlinesolution.com>
2018-03-10 18:09:07 +0530

    Change by rakhi 10/03/2018 06:08PM

--- app/src/main/java/com/carshiring/splash/SplashActivity.java
+++ app/src/main/java/com/carshiring/splash/SplashActivity.java
@@ -42,7 +42,7 @@ public class SplashActivity extends AppBaseActivity {
     ArrayList<String> langlistname,langlistcode,langlistId  ;
     String[] lan;
 
-//    hello  test
+//
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
